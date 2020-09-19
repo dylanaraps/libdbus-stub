996a6cbcfe679bd178faf4464aed78894ffa7c95
Alejandro Sior <aho@sior.be>
2020-09-20 01:17:49 +0200

    Validate all syntaxes

--- dbus/dbus-syntax.c
+++ dbus/dbus-syntax.c
@@ -4,41 +4,41 @@ dbus_bool_t dbus_validate_path(
     const char *path,
     DBusError  *error
 ) {
-    return FALSE;
+    return TRUE;
 }
 
 dbus_bool_t dbus_validate_interface(
     const char *name,
     DBusError  *error
 ) {
-    return FALSE;
+    return TRUE;
 }
 
 dbus_bool_t dbus_validate_member(
     const char *name,
     DBusError  *error
 ) {
-    return FALSE;
+    return TRUE;
 }
 
 dbus_bool_t dbus_validate_error_name(
     const char *name,
     DBusError *error
 ) {
-    return FALSE;
+    return TRUE;
 }
 
 dbus_bool_t dbus_validate_bus_name(
     const char *name,
     DBusError  *error
 ) {
-    return FALSE;
+    return TRUE;
 }
 
 dbus_bool_t dbus_validate_utf8(
     const char *alleged_utf8,
     DBusError  *error
 ) {
-    return FALSE;
+    return TRUE;
 }
 
