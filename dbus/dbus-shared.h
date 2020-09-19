d6742a2ca8ea34e1b7cb0b5c9ac2779b1a826a71
Dylan Araps <dylan.araps@gmail.com>
2020-09-19 22:57:20 +0300

    added more missing macros

--- dbus/dbus-shared.h
+++ dbus/dbus-shared.h
@@ -17,6 +17,36 @@ typedef enum  {
     DBUS_HANDLER_RESULT_NEED_MEMORY
 } DBusHandlerResult;
 
+#define DBUS_SERVICE_DBUS "org.freedesktop.DBus"
+#define DBUS_PATH_DBUS    "/org/freedesktop/DBus"
+#define DBUS_PATH_LOCAL   "/org/freedesktop/DBus/Local"
+
+#define DBUS_INTERFACE_DBUS        "org.freedesktop.DBus"
+#define DBUS_INTERFACE_CONTAINERS1 "org.freedesktop.DBus.Containers1"
+#define DBUS_INTERFACE_MONITORING  "org.freedesktop.DBus.Monitoring"
+
+#define DBUS_INTERFACE_VERBOSE        "org.freedesktop.DBus.Verbose"
+#define DBUS_INTERFACE_INTROSPECTABLE "org.freedesktop.DBus.Introspectable"
+#define DBUS_INTERFACE_PROPERTIES     "org.freedesktop.DBus.Properties"
+#define DBUS_INTERFACE_PEER           "org.freedesktop.DBus.Peer"
+
+#define DBUS_INTERFACE_LOCAL "org.freedesktop.DBus.Local"
+#define DBUS_NAME_FLAG_ALLOW_REPLACEMENT 0x1
+#define DBUS_NAME_FLAG_REPLACE_EXISTING  0x2
+#define DBUS_NAME_FLAG_DO_NOT_QUEUE      0x4
+
+#define DBUS_REQUEST_NAME_REPLY_PRIMARY_OWNER  1
+#define DBUS_REQUEST_NAME_REPLY_IN_QUEUE       2
+#define DBUS_REQUEST_NAME_REPLY_EXISTS         3
+#define DBUS_REQUEST_NAME_REPLY_ALREADY_OWNER  4
+
+#define DBUS_RELEASE_NAME_REPLY_RELEASED     1
+#define DBUS_RELEASE_NAME_REPLY_NON_EXISTENT 2
+#define DBUS_RELEASE_NAME_REPLY_NOT_OWNER    3
+
+#define DBUS_START_REPLY_SUCCESS         1
+#define DBUS_START_REPLY_ALREADY_RUNNING 2
+
 #ifdef __cplusplus
 }
 #endif
