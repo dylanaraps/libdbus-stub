1614a2809f0a6fc91e94f5aef2c926bef66336f3
Dylan Araps <dylan.araps@gmail.com>
2020-09-19 22:34:35 +0300

    slowly add missing pieces

--- dbus/dbus-protocol.h
+++ dbus/dbus-protocol.h
@@ -6,7 +6,9 @@ extern "C" {
 #endif
 
 #define DBUS_MESSAGE_TYPE_INVALID 0
-#define DBUS_TYPE_INVALID ((int) '\0')
+#define DBUS_TYPE_INVALID         ((int) '\0')
+#define DBUS_TYPE_OBJECT_PATH     ((int) 'o')
+#define DBUS_TYPE_STRING          ((int) 's')
 
 #ifdef __cplusplus
 }
