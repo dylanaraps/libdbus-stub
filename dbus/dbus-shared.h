a6ce5a60ae2d25023fc7afdb27da5c36953ce612
Dylan Araps <dylan.araps@gmail.com>
2020-09-19 14:06:32 +0300

    dbus-shared: fix header

--- dbus/dbus-shared.h
+++ dbus/dbus-shared.h
@@ -1,5 +1,19 @@
+#ifndef DBUS_SHARED_H
+#define DBUS_SHARED_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 typedef enum {
     DBUS_BUS_SESSION,
     DBUS_BUS_SYSTEM,
     DBUS_BUS_STARTER
 } DBusBusType;
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
